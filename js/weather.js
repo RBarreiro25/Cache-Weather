a176a8adc23a328457152a7aaa130f505969c892
Raphael Barreiro <126487945+RBarreiro25@users.noreply.github.com>
2023-05-02 20:38:36 -0300

    V2
    Several changes and fixes in this version.

--- js/weather.js
+++ js/weather.js
@@ -11,27 +11,52 @@ const descElement = document.querySelector('#descricao');
 const feelsLikeElement = document.querySelector('#sensacao_termica');
 const umidityElement = document.querySelector('#umidade span');
 const windElement = document.querySelector('#vento span');
-
-const divWeatherInfos = document.getElementById('weather_infos');
+const divAlertValidation = document.querySelector('.alert_validation');
+const alertValidation = document.querySelector('#alert_validation p');
+const divInfosInner = document.getElementsByClassName("infos_inner");
+const tempMinElement = document.querySelector('#temp_min');
+const tempMaxElement = document.querySelector('#temp_max');
+const sunRiseElement = document.querySelector('#nascer_sol');
+const sunSetElement = document.querySelector('#por_sol');
+const main = document.querySelector('#main');
+const contentSection = document.querySelector('#content');
+const title = document.querySelector('#title');
+const detailsElement = document.querySelectorAll('[data-details]');
+const infosInnerElement = document.querySelector('.infos_inner');
+const divInfosWrapper = document.querySelector('#infos_wrapper');
+let searchValidation = false;
 
 
 // Funções 
 const getWeatherData = async (city) => {
-  if (city === "salvador" || city === "Salvador") {
+  if (city === "salvador" || city === "Salvador" || city ==="SALVADOR") {
     city = "salvador,br";
   }
-    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKeyWeather}&lang=pt_br`;
 
-    const res = await fetch(apiWeatherUrl);
+  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKeyWeather}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherUrl);
+
+  if(res.ok) {
     const data = await res.json();
-
+    searchValidation = true;
     return data;
-}
+  } else {
+    searchValidation = false;
+  }
+  }
+
+  
 
 
 const showWeatherData = async (city) => {
-    const data = await getWeatherData(city);
+  const data = await getWeatherData(city);
 
+  if(searchValidation) {
+    if (divInfosWrapper.className == "infos_wrapper" ) {
+      divInfosWrapper.classList.toggle('active_iwrapper');
+    }
+  
     cityElement.innerText = data.name;
     tempElement.innerText = `${data.main.temp.toFixed(1).replace(".", ",")}°C`;
     weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
@@ -39,14 +64,19 @@ const showWeatherData = async (city) => {
     description = data.weather[0].description;
     descFirstCharUp = description.charAt(0).toUpperCase() + description.slice(1);
     descElement.innerText = descFirstCharUp;
-
-    feelsLikeElement.innerText = `Sensação térmica: ${Math.round(data.main.feels_like)}°C`
+  
+    feelsLikeElement.innerText = `Sensação térmica: ${data.main.feels_like.toFixed(1).replace(".", ",")}°C`
     umidityElement.innerText = `Umidade: ${data.main.humidity}%`;
-    windElement.innerText = `Velocidade dos ventos: ${data.wind.speed.toFixed(1).replace(".", ",")}km/h`;
-
-    if (divWeatherInfos.classList == 'hidden') {
-      divWeatherInfos.classList.toggle('active');
-    } 
+    windElement.innerText = `Velocidade dos ventos: ${(data.wind.speed * 3.6).toFixed(1).replace(".", ",")}km/h`;
+    tempMinElement.innerText = `${data.main.temp_min.toFixed(1).replace(".", ",")}°C`;
+    tempMaxElement.innerText = `${data.main.temp_max.toFixed(1).replace(".", ",")}°C`;
+    sunRiseElement.innerText = `${unixToTime(data.sys.sunrise)}`
+    sunSetElement.innerText = `${unixToTime(data.sys.sunset)}`
+  } else {
+    divAlertValidation.classList.add("active");
+    alertValidation.innerText = "Cidade não encontrada. Por favor, tente novamente.";
+  }
+  
     
 }
 
@@ -69,19 +99,51 @@ function retira_acentos(city) {
 	}
 
     cityNoSpecials = novacity;
-	return console.log(novacity);
+	return novacity;
 }
 
+function unixToTime(string) {
+  var date = new Date(string * 1000);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+return formattedTime;
+}
+
+function font0emToggle() {
+ let font0emToggled =  title.classList.toggle('font_0em');
+
+ return font0emToggled
+}
 
 // Eventos
 searchBtn.addEventListener("click", (event) => {
   event.preventDefault();
-
-  const city = cityInput.value;
+  alertValidation.innerText = "";
+  divAlertValidation.classList.remove("active");
   
-  showWeatherData(city);
-  cityInput.value = '';
-  retira_acentos(city);
+  if (cityInput.value !== "" && cityInput.value.length > 2) {
+    const city = cityInput.value;
+    showWeatherData(city);
+    cityInput.value = '';
+    retira_acentos(city);
+
+  if (title.className == "title") {
+    title.classList.toggle('hidden');
+    setTimeout(font0emToggle, 400);
+    contentSection.classList.toggle('move_up');
+  }
+
+  } else {
+    divAlertValidation.classList.add("active");
+    alertValidation.innerText = "Pesquisa vazia ou muito curta. Por favor, tente novamente.";
+  }
+
+  
 })
 
 cityInput.addEventListener("keypress", function(event) {
@@ -90,3 +152,12 @@ cityInput.addEventListener("keypress", function(event) {
     searchBtn.click();
   }
 });
+
+detailsElement.forEach(function(e) {
+
+  e.addEventListener("click", function() {
+    infosInnerElement.classList.toggle('flip_transition');
+  })
+})
+
+
